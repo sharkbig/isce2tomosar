bd7d8da4ac411ce42e141117f74c256538b18046
Jun-Yan C <james15156@gmail.com>
2023-07-24 02:34:52 +0800

    modified the data structure

--- makeH5Stack.py
+++ makeH5Stack.py
@@ -11,23 +11,33 @@ def makeSLCStack(slcFolder,h5Name,subset=[]):
 
     if len(subset)==4:
         p,q,m,n=subset
-        y=q-p
-        x=n-m
+        ysize=q-p
+        xsize=n-m
     elif len(subset) == 0:
-        p=m=0;q=y;n=x
+        p=m=0
+        ysize=q=y
+        xsize=n=x
     else:
         print('wrong subset area\nLoading hole image')
-        p=m=0;q=y;n=x
+        p=m=0
+        ysize=q=y
+        xsize=n=x
+    print(ysize,xsize)
+    
     # create dataset
     ds=h5py.File(h5Name,'w')
-    realpart=ds.create_dataset('slc_real',(nslc,y,x),dtype=np.float32)
-    imagpart=ds.create_dataset('slc_imag',(nslc,y,x),dtype=np.float32)
-    
+    # realpart=ds.create_dataset('slc_real',(nslc,ysize,xsize),dtype=np.float32)
+    # imagpart=ds.create_dataset('slc_imag',(nslc,ysize,xsize),dtype=np.float32)
+    slcLayer=ds.create_dataset('slc',(nslc*2,ysize,xsize),chunks=(1,ysize,xsize),
+        dtype=np.float32)
+
     for i in range(nslc):
         ds,*_ = loadSLC(dates[i])
         cpx=ds.ReadAsArray()
-        realpart[i,p:q,m:n]=cpx.real
-        imagpart[i,p:q,m:n]=cpx.imag
+        # realpart[i,:,:]=cpx.real[p:q,m:n]
+        # imagpart[i,:,:]=cpx.imag[p:q,m:n]
+        slcLayer[i,:,:]=cpx.real[p:q,m:n]
+        slcLayer[i+nslc,:,:]=cpx.imag[p:q,m:n]
         ds=None 
         print('slc process:',dates[i])
     
