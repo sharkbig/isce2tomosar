54099107b8f88c1fd4c9dec582cdc3f19e07cf45
Jun-Yan C <james15156@gmail.com>
2023-07-30 14:26:00 +0800

    add residual

--- tomoInversion.py
+++ tomoInversion.py
@@ -11,13 +11,13 @@ import cupy
 # projection information
 projectFolder='/RUNDATA_16804/junyan1998/Chehualin/H4-10/crop_H4-10'
 stackName='microStack.h5'
-subset=[2000,2300,3800,4050] #[row_start,row_end,col_start,col_end]
+subset=[1800,2300,3800,4050] #[row_start,row_end,col_start,col_end]
 subsetStack=1
 overwriteStack=0
 dS=3
 # satelite information
 wavelen=0.031
-H=620e3 
+H=620e3
 win=5
 
 
@@ -82,14 +82,14 @@ del imagRef, realRef, _real, _imag
 
 
 # load incident angle and slantrange calculation
-inc=utils.loadGeom(geomFolder)
+inc=utils.loadGeom(geomFolder,'los.rdr')
 incAve=np.average(inc)*3.14/180
 r0=utils.calcSR(incAve,H)
 
 # calculate baseline aperture 
 deltaS=wavelen*r0/2/(np.max(bperp)-np.min(bperp))
 dS=min(dS,deltaS)
-# db=utils.baselineInverval(bperp)
+# db=utils.cbaselineInverval(bperp)
 # db=np.std(bperp)
 # Srange=wavelen*r0/4/3.14/(nslc*20)**0.5/db
 Srange=80
@@ -106,24 +106,33 @@ steeringH=np.conj(steering.T)
 
 
 # normalize observation 
+print('apply filter')
 cpx=real+1j*imag
-gaussian=gpu_moving_average_2d(cpx,3,nslc)
+gaussian=gpu_moving_average_2d(cpx,win,nslc)
 da=cupy.asnumpy(da_gpu(gaussian))
 
 
 # start inversion
 print('start inversion ... ')
 
-# method 1: mvdr beamforming
+### method 1: mvdr beamforming
+
 tomography=gpuBFinversion(gaussian,steering)
+predict=np.einsum('ijk,lk->ijl',tomography,steering)
+residual=np.sum(gaussian.transpose([1,2,0]).conj().get()*predict,axis=2)
+tomography=tomography.real
 
 
-# method 2: svd beamforming
+# method 2: tsvd beamforming
 
 # import svdBF 
 # gaussian=gaussian.get().reshape(nslc,-1)
-# tomography=svdBF.svdBF(gaussian,steering,1)
+# tomography=svdBF.svdBF(gaussian,steering,1) # 1 for truncated threshold
+# predict=np.einsum('ij,kj->ik',tomography,steering)
+# residual=np.sum(gaussian.T.conj()*predict,axis=1)
 # tomography=tomography.reshape(lns,width,ntrial).real
+# residual=residual.reshape(lns,width)
+# tomography=tomography
 
 
 # method 3: Compressive sensing method
@@ -138,25 +147,39 @@ tomography=gpuBFinversion(gaussian,steering)
 # tomography=np.abs(tomography.reshape(lns,width,ntrial))
 
 
-#################################3
+#################################
 # verify result 
 
 print('result output')
 inten=np.log(np.average((real**2+imag**2)**0.5,axis=0))
 output=trial[0,np.argmax(tomography,axis=2)]*np.sin(incAve)
 output[da>0.6]=np.nan
+residual[da>0.6]=np.nan
 utils.exportPointHeight(output,lon,lat)
 
 
-plt.subplot(121)
+
+plt.figure(figsize=(16,5))
+plt.subplot(141)
+plt.imshow(inten,cmap="gray")
+plt.title('intensity')
+plt.colorbar(orientation= "horizontal")
+plt.subplot(142)
+plt.imshow(da,cmap="jet")
+plt.clim(0.4,1)
+plt.title('dispersion index')
+plt.colorbar(orientation= "horizontal")
+plt.subplot(143)
 plt.imshow(output,cmap='jet')
 plt.colorbar(orientation= "horizontal")
-plt.subplot(122)
-plt.imshow(inten,cmap="gray")
+plt.title('tomo-height estimation')
+plt.subplot(144)
+plt.imshow(np.log10(np.abs(residual)),cmap="rainbow")
+plt.title('phase residule (log scale)')
 plt.colorbar(orientation= "horizontal")
+
 plt.savefig('csinverse/aveInt02')
 plt.close()
-
 
 
 for testLine in range(0,real.shape[1],10):
