f036479e8a72fe368f498e6dc9455e58a7266ab4
Jun-Yan C <james15156@gmail.com>
2023-08-03 02:16:31 +0800

    coherence calculation

--- tomoInversion.py
+++ tomoInversion.py
@@ -1,11 +1,12 @@
 #!/bin/env python
 import numpy as np
 import utils
+from utils import cdot 
 import matplotlib.pyplot as plt
 import os
 from gpuInversion import *
-import cupy
-
+import cupy as cp
+from makeH5Stack import *
 
 
 # projection information
@@ -14,7 +15,7 @@ stackName='microStack.h5'
 subset=[1800,2300,3800,4050] #[row_start,row_end,col_start,col_end]
 subsetStack=1
 overwriteStack=0
-dS=3
+dS=5
 # satelite information
 wavelen=0.031
 H=620e3
@@ -30,27 +31,38 @@ slcFolder=os.path.join(projectFolder,'SLC')
 if os.path.exists(stackName) and not overwriteStack:
     pass
 else:
-    from makeH5Stack import makeSLCStack
+    
     print('make SLC stack')
     if subsetStack:
         makeSLCStack(slcFolder,stackName,subset)
     else:    
         makeSLCStack(slcFolder,stackName)
 
-# load longtitude and latitdue
-lon=utils.loadGeom(geomFolder,'lon.rdr',subset)
-lat=utils.loadGeom(geomFolder,'lat.rdr',subset)
-
-# load Data 
-print('Loading stack dataset\n')
+print('Loading SLC stack dataset')
 
 if subsetStack:
-    real,imag=utils.loadStack(stackName)
+    real,imag=loadSLCStack(stackName)
 else:
-    real,imag=utils.loadStack(stackName,subset)
+    real,imag=loadSLCStack(stackName,subset)
+
+
+# load longtitude and latitdue and slant range 
+print('Loading geometry dataset')
+if  not os.path.exists('geom.h5') or overwriteStack:
+    makeGeomStack(geomFolder,'geom.h5',subset)
+
+lon=loadGeomStack('geom.h5','lon')
+lat=loadGeomStack('geom.h5','lat')
+inc=loadGeomStack('geom.h5','inc')
+
+incAve=np.average(inc)*3.14/180
+r0=utils.calcSR(incAve,H)
+
+# load Data 
 
 
 # load baselines 
+print('Averaging baseline')
 if os.path.exists('bp.npy'):
     bperp=np.load('bp.npy')
 else:
@@ -81,18 +93,14 @@ bperp=bperp-bperp[masterIx]
 del imagRef, realRef, _real, _imag
 
 
-# load incident angle and slantrange calculation
-inc=utils.loadGeom(geomFolder,'los.rdr')
-incAve=np.average(inc)*3.14/180
-r0=utils.calcSR(incAve,H)
 
 # calculate baseline aperture 
 deltaS=wavelen*r0/2/(np.max(bperp)-np.min(bperp))
-dS=min(dS,deltaS)
-# db=utils.cbaselineInverval(bperp)
+dS=min(dS,deltaS/2)
+# db=utils.baselineInverval(bperp)
 # db=np.std(bperp)
 # Srange=wavelen*r0/4/3.14/(nslc*20)**0.5/db
-Srange=80
+Srange=60
 print(Srange,dS)
 trial=np.arange(-Srange/2,Srange/2,dS).reshape([1,-1])
 ntrial=trial.shape[1]
@@ -108,34 +116,56 @@ steeringH=np.conj(steering.T)
 # normalize observation 
 print('apply filter')
 cpx=real+1j*imag
-gaussian=gpu_moving_average_2d(cpx,win,nslc)
-da=cupy.asnumpy(da_gpu(gaussian))
+if win==1:
+    gaussian=cpx
+else: 
+    gaussian=gpu_moving_average_2d(cpx,win,nslc)
+
+da=cp.asnumpy(da_gpu(gaussian))
 
 
 # start inversion
 print('start inversion ... ')
 
-### method 1: mvdr beamforming
+### method 1: mvdr beamforming (on GPU)
 
 tomography=gpuBFinversion(gaussian,steering)
-predict=np.einsum('ijk,lk->ijl',tomography,steering)
-residual=np.sum(gaussian.transpose([1,2,0]).conj().get()*predict,axis=2)
-tomography=tomography.real
+k=cp.argmax(tomography,axis=2)
+s_gpu=cp.asarray(steering)
+
+# calculate coherence
+sig=cp.angle(gaussian)
+sig=cp.exp(1j*sig)
+L_g=np.einsum('ij,jkl->ikl',s_gpu.conj().T,sig)
+L_k=np.einsum('ij,jkl->ikl',s_gpu.conj().T,s_gpu[:,k])
+coh=cp.sum(L_g*L_k.conj(),axis=0)/  \
+    cp.sum(L_k*L_k.conj(),axis=0)**0.5/  \
+    cp.sum(L_g*L_g.conj(),axis=0)**0.5
+
+# copy data back
+coh=cp.abs(coh).get()
+tomography=tomography.get().real
 
 
-# method 2: tsvd beamforming
+#### scatter distribution plot
+# count,nbin=np.histogram(coh.flatten(),bins=50)
+# cdf=np.cumsum(count/sum(count))
+# plt.plot(nbin[1:],cdf)
+# plt.savefig('coh_cumsum')
+
+
+### method 2: tsvd beamforming
 
 # import svdBF 
 # gaussian=gaussian.get().reshape(nslc,-1)
-# tomography=svdBF.svdBF(gaussian,steering,1) # 1 for truncated threshold
+# tomography=svdBF.svdBF(gaussian,steering,1e-3) # 1 for truncated threshold
 # predict=np.einsum('ij,kj->ik',tomography,steering)
 # residual=np.sum(gaussian.T.conj()*predict,axis=1)
 # tomography=tomography.reshape(lns,width,ntrial).real
 # residual=residual.reshape(lns,width)
-# tomography=tomography
 
 
-# method 3: Compressive sensing method
+### method 3: Compressive sensing method
 
 # from CSInversion import cs_omp
 # gaussian=gaussian.reshape(nslc,-1).get()
@@ -148,13 +178,14 @@ tomography=tomography.real
 
 
 #################################
-# verify result 
 
+# verify result 
 print('result output')
 inten=np.log(np.average((real**2+imag**2)**0.5,axis=0))
 output=trial[0,np.argmax(tomography,axis=2)]*np.sin(incAve)
-output[da>0.6]=np.nan
-residual[da>0.6]=np.nan
+
+output[coh<0.6]=np.nan
+coh[coh<0.6]=np.nan
 utils.exportPointHeight(output,lon,lat)
 
 
@@ -174,17 +205,20 @@ plt.imshow(output,cmap='jet')
 plt.colorbar(orientation= "horizontal")
 plt.title('tomo-height estimation')
 plt.subplot(144)
-plt.imshow(np.log10(np.abs(residual)),cmap="rainbow")
+plt.imshow(coh,cmap="rainbow")
 plt.title('phase residule (log scale)')
 plt.colorbar(orientation= "horizontal")
-
+plt.tight_layout()
 plt.savefig('csinverse/aveInt02')
 plt.close()
 
-
+dist=np.arange(width)
+d,z=np.meshgrid(dist,trial*np.sin(incAve))
 for testLine in range(0,real.shape[1],10):
     plt.figure(figsize=(10,4))
-    plt.pcolor(tomography[testLine,...].T,cmap='rainbow')
+    
+    plt.pcolor(d,z,tomography[testLine,...].T,cmap='rainbow')
+    plt.plot(output[testLine,:],'k+')
     plt.colorbar(orientation='horizontal')
     plt.savefig(f'csinverse/profile{testLine}')
     plt.close()
